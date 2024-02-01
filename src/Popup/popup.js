872fa5c1a31aaf92b260d973051e6ebb002f8506
Edin Muhic <edin.muhic@forsta.com>
2024-02-01 17:18:35 +0100

    Refactor code

--- src/Popup/popup.js
+++ src/Popup/popup.js
@@ -1,8 +1,8 @@
 /* global chrome */
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 import '../Styles/popup.css';
-import { saveSnippet, loadSnippets } from '../Utils/snippetActions';
+import { saveSnippet, toggleFavoriteStatus, copySnippet, deleteSnippet, getHandleAddingName, getHandleAddingContent, getToggleSnippetsVisibility } from '../Utils/snippetActions';
 
 function Popup() {
     const [snippetName, setSnippetName] = useState('');
@@ -10,56 +10,9 @@ function Popup() {
     const [snippets, setSnippets] = useState([]);
     const [snippetsVisible, setSnippetsVisible] = useState(false);
 
-    const handleNameChange = (event) => {
-        setSnippetName(event.target.value);
-    };
-
-    const handleContentChange = (event) => {
-        setSnippetContent(event.target.value);
-    };
-
-    const toggleFavoriteStatus = (snippetName) => {
-        const updatedSnippets = snippets.map(snippet => {
-            if (snippet.name === snippetName) {
-                return { ...snippet, isFavorite: !snippet.isFavorite };
-            }
-            return snippet;
-        });
-    
-        setSnippets(updatedSnippets);
-        chrome.storage.local.set({ snippets: updatedSnippets }, () => {
-            console.log('Updated favorite status');
-        });
-    };
-    
-    const copySnippet = (snippetContent) => {
-        navigator.clipboard.writeText(snippetContent)
-            .then(() => {
-                console.log('Snippet content copied to clipboard');
-            })
-            .catch(err => {
-                console.error('Failed to copy snippet: ', err);
-            });
-    };
-    
-    const toggleSnippetEditMode = (snippetName) => {
-        // Implement the logic to enable editing mode for the snippet
-    };
-    
-    const deleteSnippet = (snippetName) => {
-        const updatedSnippets = snippets.filter(snippet => snippet.name !== snippetName);
-        chrome.storage.local.set({ snippets: updatedSnippets }, () => {
-            setSnippets(updatedSnippets);
-            console.log('Snippet deleted');
-        });
-    };
-
-    const toggleSnippetsVisibility = () => {
-        if (!snippetsVisible) {
-            loadSnippets(setSnippets);
-        }
-        setSnippetsVisible(!snippetsVisible);
-    };
+    const handleAddingName = getHandleAddingName(setSnippetName);
+    const handleAddingContent = getHandleAddingContent(setSnippetContent);
+    const toggleSnippetsVisibility = getToggleSnippetsVisibility(snippetsVisible, setSnippetsVisible, setSnippets);
 
     const onDragEnd = (result) => {
         if (!result.destination) {
@@ -81,13 +34,13 @@ function Popup() {
                 type="text"
                 placeholder="Name"
                 value={snippetName}
-                onChange={handleNameChange}
+                onChange={handleAddingName}
             />
             <textarea
                 id="snippetContent"
                 placeholder="Content"
                 value={snippetContent}
-                onChange={handleContentChange}
+                onChange={handleAddingContent}
             />
             <p id="errorMessage"></p>
             <div className="button-container">
@@ -104,7 +57,7 @@ function Popup() {
                     onClick={toggleSnippetsVisibility}
                 />
             </div>
-            
+
             {snippetsVisible && (
                 <DragDropContext onDragEnd={onDragEnd}>
                     <Droppable droppableId="snippetsDroppable">
@@ -114,19 +67,19 @@ function Popup() {
                                     <Draggable key={snippet.name} draggableId={snippet.name} index={index}>
                                         {(provided) => (
                                             <div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps} className="snippet-item">
-                                                <div>{snippet.name}</div>
-                                                <span className="favorite-button material-icons" onClick={() => toggleFavoriteStatus(snippet.name)}>
-                                                    {snippet.isFavorite ? 'favorite' : 'favorite_border'}
-                                                </span>
-                                                <span className="edit-button material-icons" onClick={() => toggleSnippetEditMode(snippet.name)}>
-                                                    edit
-                                                </span>
-                                                <span className="copy-button material-icons" onClick={() => copySnippet(snippet.content)}>
-                                                    content_copy
-                                                </span>
-                                                <span className="delete-button material-icons" onClick={() => deleteSnippet(snippet.name)}>
-                                                    delete
-                                                </span>
+                                                    <div>{snippet.name}</div>
+                                                        <span className="favorite-button material-icons" onClick={() => toggleFavoriteStatus(snippet.name, snippets, setSnippets)}>
+                                                            {snippet.isFavorite ? 'favorite' : 'favorite_border'}
+                                                        </span>
+                                                        <span className="edit-button material-icons">
+                                                            edit
+                                                        </span>
+                                                        <span className="copy-button material-icons" onClick={() => copySnippet(snippet.content)}>
+                                                            content_copy
+                                                        </span>
+                                                        <span className="delete-button material-icons" onClick={() => deleteSnippet(snippet.name, snippets, setSnippets)}>
+                                                            delete
+                                                        </span>
                                             </div>
                                         )}
                                     </Draggable>
--- src/Utils/snippetActions.js
+++ src/Utils/snippetActions.js
@@ -1,4 +1,17 @@
 /* global chrome */
+
+export const getHandleAddingName = (setSnippetName) => {
+    return (event) => {
+        setSnippetName(event.target.value);
+    };
+};
+
+export const getHandleAddingContent = (setSnippetContent) => {
+    return (event) => {
+        setSnippetContent(event.target.value);
+    };
+};
+
 export const saveSnippet = (snippets, setSnippets, snippetName, snippetContent, setSnippetName, setSnippetContent) => {
     if (snippets.some(snippet => snippet.name === snippetName)) {
         console.error('Snippet name must be unique.');
@@ -24,4 +37,45 @@ export const loadSnippets = (setSnippets) => {
         console.log('Loaded snippets:', loadedSnippets);
         setSnippets(loadedSnippets);
     });
+};
+
+export const toggleFavoriteStatus = (snippetName, snippets, setSnippets) => {
+    const updatedSnippets = snippets.map(snippet => {
+        if (snippet.name === snippetName) {
+            return { ...snippet, isFavorite: !snippet.isFavorite };
+        }
+        return snippet;
+    });
+
+    setSnippets(updatedSnippets);
+    chrome.storage.local.set({ snippets: updatedSnippets }, () => {
+        console.log('Updated favorite status');
+    });
+};
+
+export const copySnippet = (snippetContent) => {
+    navigator.clipboard.writeText(snippetContent)
+        .then(() => {
+            console.log('Snippet content copied to clipboard');
+        })
+        .catch(err => {
+            console.error('Failed to copy snippet: ', err);
+        });
+};
+
+export  const deleteSnippet = (snippetName, snippets, setSnippets) => {
+    const updatedSnippets = snippets.filter(snippet => snippet.name !== snippetName);
+    chrome.storage.local.set({ snippets: updatedSnippets }, () => {
+        setSnippets(updatedSnippets);
+        console.log('Snippet deleted');
+    });
+};
+
+export const getToggleSnippetsVisibility = (snippetsVisible, setSnippetsVisible, setSnippets) => {
+    return () => {
+        if (!snippetsVisible) {
+            loadSnippets(setSnippets);
+        }
+        setSnippetsVisible(!snippetsVisible);
+    };
 };
